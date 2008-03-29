2c4bae1c7f372419f32ee95e28cdc99ff3fd7eb3
sevcsik <sevcsik@af6d5b84-8049-0410-9365-81adf330aa37>
2008-03-29 15:33:21 +0000

    Initial import.

---
+++ eyesight/src/bin/conf.h
@@ -0,0 +1,9 @@
+#ifndef CONF_H_
+#define CONF_H_
+
+struct
+{
+	char *theme_file;
+} main_config;
+
+#endif /*CONF_H_*/
---
+++ eyesight/src/bin/main.c
@@ -0,0 +1,20 @@
+#include "Eyesight.h"
+
+char * parse_args(int argc, char **argv)
+{
+	// TODO: parse_args()
+}
+
+int main(int argc, char **argv)
+{
+    // gettext stuff
+    setlocale(LC_ALL, "");
+    bindtextdomain(PACKAGE, LOCALEDIR);
+    textdomain(PACKAGE);
+	
+	if (!init_libs(argc, argv)) exit(-1);
+	
+	if (parse_args(argc, argv));
+	
+	ecore_main_loop_begin();
+}
