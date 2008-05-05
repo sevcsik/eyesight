6f4274073c37a3b19aca9e8454fed524d14baf55
sevcsik <sevcsik@af6d5b84-8049-0410-9365-81adf330aa37>
2008-05-05 20:31:58 +0000

    Added startup_error list, and support in parse_args()

--- trunk/eyesight/src/bin/error.h
+++ trunk/eyesight/src/bin/error.h
@@ -1,7 +1,11 @@
 #ifndef ERROR_H_
 #define ERROR_H_
 
-//#define ERROR_ARG _("%s option requires an argument, but none has been given.\n")
-#define ERROR_THEME _("Couldn't open %s as theme file. Check that the file exists and it's compatible with this Eyesight version!\n")
+#include <libintl.h>
+#define _(STRING) gettext(STRING)
+
+//#define ERROR_ARG_NOARG _("-%s option requires an argument, but none has been given.")
+#define ERROR_ARG_UNKNOWN _("-%s is an unknown option, or it needs an argument. See Help for details.")
+#define ERROR_THEME _("Couldn't open %s as theme file. Check that the file exists and it's compatible with this Eyesight version!")
 
 #endif /*ERROR_H_*/
