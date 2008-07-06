97d8737ab3054db3790b861e7a628b5e262c099f
sevcsik <sevcsik@af6d5b84-8049-0410-9365-81adf330aa37>
2008-07-06 20:18:56 +0000

    Added fullscreen button (FIXME: not receiving mouse events)

--- trunk/eyesight2/src/plugins/pdf/toolbar.h
+++ trunk/eyesight2/src/plugins/pdf/toolbar.h
@@ -8,20 +8,34 @@ typedef enum _Pdf_Toolbar1_Icon
    PREV_PAGE = 0,
    // Page no. input field will be here
    NEXT_PAGE = 3
-} Pdf_Toolbar1_Icon;
+}
+Pdf_Toolbar1_Icon;
+
+typedef enum _Pdf_Toolbar2_Icon
+{
+   FULLSCREEN = 0
+}
+Pdf_Toolbar2_Icon;
 
 void add_toolbar1_icon(Pdf_Toolbar1_Icon icon, Evas_Object *controls);
+
+void add_toolbar2_icon(Pdf_Toolbar2_Icon icon, Evas_Object *controls);
 
 void toolbar_icon_resize_cb(void *data, Evas *evas, Evas_Object *obj,
                             void *event_info);
 
-void page_next_clicked(void *data, Evas_Object *o, const char *emission, 
+void page_next_clicked(void *data, Evas_Object *o, const char *emission,
                        const char *source);
 
-void page_prev_clicked(void *data, Evas_Object *o, const char *emission, 
+void page_prev_clicked(void *data, Evas_Object *o, const char *emission,
                        const char *source);
+
+void fullscreen_clicked(void *data, Evas_Object *icon, const char *emission,
+                        const char *source);
 
 void toolbar_button_resize_cb(void *_data, Evas *evas, Evas_Object *controls,
-                               void *event_info);
+                              void *event_info);
+
+void setup_toolbar(Evas_Object *controls);
 
 #endif /* TOOLBAR_H_ */
