188c9cd43f9297e4d6652ec3a3165d73775be296
sevcsik <sevcsik@af6d5b84-8049-0410-9365-81adf330aa37>
2008-07-06 20:18:56 +0000

    Added fullscreen button (FIXME: not receiving mouse events)

--- eyesight2/src/plugins/pdf/pdf.h
+++ eyesight2/src/plugins/pdf/pdf.h
@@ -9,14 +9,14 @@ typedef struct _Pdf_File_Data
    Evas_Object *page;
    Evas_Object *tmp_border; // Hidden outside the window for animations
    Evas_Object *tmp_page;
-} Pdf_File_Data;
+}
+Pdf_File_Data;
 
 void identify(char **name, char **version, char **email);
 char init(void **plugin_data);
 char open_file(void **_plugin_data, char *filename, Evas_Object *main_window,
-              Evas *evas);
+               Evas *evas);
 void show(void **plugin_data, char *filename, Evas *evas);
-void setup_toolbar(Evas_Object *controls);
 void page_resize_cb(void *_data, Evas *evas, Evas_Object *controls, void *event_info);
 int page_resize_cb_render_timer(void *data);
 
