43075ad226e399d99d1a4d6b5ccebeef7cc9b779
sevcsik@gmail.com <sevcsik@gmail.com@af6d5b84-8049-0410-9365-81adf330aa37>
2008-07-28 00:01:07 +0000

    added drawer

--- eyesight/src/plugins/document/defines.h
+++ eyesight/src/plugins/document/defines.h
@@ -1,6 +1,10 @@
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
 #if defined PDF
 /* Defines for PDF plugin */
 #include <Epdf.h>
+#include <esmart_pdf.h>
 
 // Functions
 #define doc_add(X)               esmart_pdf_add(X)
@@ -10,12 +14,20 @@
 #define doc_page_set(X, Y)       esmart_pdf_page_set(X, Y)
 #define doc_page_get(X)          esmart_pdf_page_get(X)
 #define doc_size_get(X, Y, Z)    esmart_pdf_size_get(X, Y, Z)
-#define doc_scale_set(X, Y, Z)   esmart_pdf_scale_set(X, Y, Z)
-#define doc_scale_get(X, Y, Z)   esmart_pdf_scale_get(X, Y, Z)
 #define doc_document_get(X)      esmart_pdf_pdf_document_get(X)
 #define doc_page_obj_get(X)      esmart_pdf_pdf_page_get(X)
 #define doc_page_count_get(X)    epdf_document_page_count_get(X)
 #define doc_render(X)            esmart_pdf_render(X)
+
+#define doc_scale_set(X, Y)      esmart_pdf_scale_set(X, Y, Y)
+
+static double inline
+doc_scale_get(Evas_Object *o)
+{
+   double scale;
+   esmart_pdf_scale_get(o, &scale, NULL);
+   return scale;
+}
 
 // Types
 typedef Epdf_Document         Doc_Document;
@@ -24,6 +36,7 @@ typedef Epdf_Page             Doc_Page;
 #elif defined DVI
 /* Defines for DVI plugin */
 #include <Edvi.h>
+#include <esmart_dvi.h>
 
 // Functions
 #define doc_add(X)               esmart_dvi_add(X)
@@ -33,15 +46,17 @@ typedef Epdf_Page             Doc_Page;
 #define doc_page_set(X, Y)       esmart_dvi_page_set(X, Y)
 #define doc_page_get(X)          esmart_dvi_page_get(X)
 #define doc_size_get(X, Y, Z)    esmart_dvi_size_get(X, Y, Z)
-#define doc_mag_set(X, Y)        esmart_dvi_mag_set(X, Y)
-#define doc_mag_get(X)           esmart_dvi_mag_get(X)
 #define doc_document_get(X)      esmart_dvi_dvi_document_get(X)
 #define doc_page_obj_get(X)      esmart_dvi_dvi_page_get(X)
 #define doc_page_count_get(X)    edvi_document_page_count_get(X)
 #define doc_render(X)            esmart_dvi_render(X)
+
+#define doc_scale_set(X, Y)      esmart_dvi_mag_set(X, Y)
+#define doc_scale_get(X)         esmart_dvi_mag_get(X)
 
 // Types
 typedef Edvi_Document         Doc_Document;
 typedef Edvi_Page             Doc_Page;
 
 #endif /* PDF & DVI*/
+#endif /* DEFINES_H_ */
--- eyesight/src/plugins/document/document.h
+++ eyesight/src/plugins/document/document.h
@@ -1,24 +1,25 @@
-#ifndef PDF_H_
-#define PDF_H_
+#ifndef DOCUMENT_H_
+#define DOCUMENT_H_
 
 #include <Evas.h>
 
-typedef struct _Pdf_File_Data
+typedef struct _Document_File_Data
 {
    Evas_Object *border;
    Evas_Object *page;
    Evas_Object *tmp_border; // Hidden outside the window for animations
    Evas_Object *tmp_page;
 }
-Pdf_File_Data;
+Document_File_Data;
 
 void identify(char **name, char **version, char **email);
 char init(void **plugin_data);
 char open_file(void **_plugin_data, char *filename, Evas_Object *main_window,
                Evas *evas);
 void show(void **plugin_data, char *filename, Evas *evas);
+void shutdown(void **plugin_data, Evas *evas);
 void page_resize_cb(void *_data, Evas *evas, Evas_Object *controls, void *event_info);
 int page_resize_cb_render_timer(void *data);
 void mousewheel_cb(void *_data, Evas *evas, Evas_Object *controls, void *event_info);
 
-#endif /*PDF_H*/
+#endif /*DOCUMENT_H*/
