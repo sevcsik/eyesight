5ac8ff571c6de36b69e8713e6ce76c66157a75a0
sevcsik@gmail.com <sevcsik@gmail.com@af6d5b84-8049-0410-9365-81adf330aa37>
2008-08-02 22:02:37 +0000

    implemented zoom

--- eyesight/src/plugins/document/document.c
+++ eyesight/src/plugins/document/document.c
@@ -4,8 +4,10 @@
 #include "toolbar.h"
 #include "plugin.h"
 #include "animations.h"
+#include "view.h"
 #include <Ecore.h>
 #include <Ecore_Data.h>
+#include <Ecore_Evas.h>
 #include <Esmart/Esmart_Container.h>
 #include <Edje.h>
 #include <Efreet.h>
@@ -35,19 +37,19 @@ Document_Page_Resize_Cb_Data;
 void
 identify(char **name, char **version, char **email)
 {
-#if defined PDF
+#   if defined PDF
    // TODO: document.c: i18n
    *name = "PDF viewer using epdf";
    *version = "0.1";
    *email = "sevcsik@gmail.com";
 
-#elif defined DVI
+#   elif defined DVI
 
    *name = "DVI viewer using edvi";
    *version = "0.1";
    *email = "sevcsik@gmail.com";
 
-#endif
+#   endif
 }
 
 char
@@ -56,15 +58,15 @@ init(void **plugin_data)
    if (PLUGIN_API_VERSION != API_VERSION_REQ)
       return PLUGIN_INIT_API_MISMATCH;
 
-   *plugin_data = malloc(sizeof(Document_Plugin_Data));
-   ((Document_Plugin_Data *)*plugin_data)->files = ecore_hash_new(NULL, NULL);
-   if (!((Document_Plugin_Data *)*plugin_data)->files)
+   *plugin_data = malloc(sizeof (Document_Plugin_Data));
+   ((Document_Plugin_Data *) * plugin_data)->files = ecore_hash_new(NULL, NULL);
+   if (!((Document_Plugin_Data *) * plugin_data)->files)
       return PLUGIN_INIT_FAIL;
 
-#if defined DVI
+#   if defined DVI
 
    edvi_init(300, "cx", 4, 1.0, 1.0, 255, 255, 255, 255, 0, 0, 0);
-#endif
+#   endif
 
    efreet_init();
    // No need to init epdf
@@ -79,9 +81,9 @@ shutdown(void **plugin_data, Evas *evas)
    evas_object_del(evas_object_name_find(evas, "tmp_page"));
    evas_object_del(evas_object_name_find(evas, "tmp_border"));
 
-#if defined DVI
+#   if defined DVI
    edvi_shutdown();
-#endif
+#   endif
 }
 
 char
@@ -105,13 +107,13 @@ open_file(void **_plugin_data, char *filename, Evas_Object *main_window,
    if (!doc_init(page))
       return 0;
 
-#if defined DVI
+#   if defined DVI
 
    printf("Trying to open %s with dvi plugin... ", filename);
-#elif defined PDF
+#   elif defined PDF
 
    printf("Trying to open %s with pdf plugin... ", filename);
-#endif
+#   endif
 
    if (!doc_file_set(page, filename)) // Check that file open was successful
    {
@@ -124,7 +126,7 @@ open_file(void **_plugin_data, char *filename, Evas_Object *main_window,
    evas_object_name_set(page, "page");
 
    // Set up border
-   edje_object_file_get(main_window, (const char **)&themefile, NULL);
+   edje_object_file_get(main_window, (const char **) & themefile, NULL);
    border = edje_object_add(evas);
    edje_object_file_set(border, themefile, "eyesight/border_opaque");
    edje_object_part_swallow(border, "eyesight/border_opaque/content", page);
@@ -148,7 +150,7 @@ open_file(void **_plugin_data, char *filename, Evas_Object *main_window,
 
    // Add callback to resize callback list
    // TODO: pdf.c: Free resize_cb_data somewhere
-   resize_cb_data = malloc(sizeof(Document_Page_Resize_Cb_Data));
+   resize_cb_data = malloc(sizeof (Document_Page_Resize_Cb_Data));
    resize_cb_data->page = page;
    resize_cb_data->border = border;
    resize_cb_data->top_margin = top_margin;
@@ -157,7 +159,7 @@ open_file(void **_plugin_data, char *filename, Evas_Object *main_window,
    resize_cb_data->right_margin = right_margin;
    resize_cb_data->render_timer = NULL;
 
-   cbdata = malloc(sizeof(Controls_Resize_Cbdata));
+   cbdata = malloc(sizeof (Controls_Resize_Cbdata));
    cbdata->data = resize_cb_data;
    cbdata->func = page_resize_cb;
 
@@ -190,7 +192,7 @@ open_file(void **_plugin_data, char *filename, Evas_Object *main_window,
    doc_render(page);
 
    // Save work
-   file_data = malloc(sizeof(Document_File_Data));
+   file_data = malloc(sizeof (Document_File_Data));
    file_data->page = page;
    file_data->border = border;
    file_data->tmp_page = NULL;
@@ -214,7 +216,7 @@ show(void **_plugin_data, char *filename, Evas *evas)
    file_data = ecore_hash_get(hash, filename);
    border = file_data->border;
    page = file_data->page;
-   show_anim_data = malloc(sizeof(Document_Show_Anim_Data));
+   show_anim_data = malloc(sizeof (Document_Show_Anim_Data));
    show_anim_data->evas = evas;
    show_anim_data->object = border;
 
@@ -254,29 +256,59 @@ page_resize_cb(void *_data, Evas *evas, Evas_Object *controls, void *event_info)
    int ew, eh, nw, nh, h_margins, v_margins;
    double scale;
    Evas_Object *page, *border;
+   Document_View_Mode *mode;
 
    page = evas_object_name_find(evas, "page");
    border = evas_object_name_find(evas, "border");
+   mode = ecore_evas_data_get(ecore_evas_ecore_evas_get(evas), "viewmode");
 
    h_margins = data->left_margin + data->right_margin;
    v_margins = data->top_margin + data->bottom_margin;
 
    evas_object_geometry_get(controls, NULL, NULL, &ew, &eh);
    doc_size_get(page, &nw, &nh);
-   scale = (((double)eh - v_margins) / (double)nh <   \
-            ((double)ew - h_margins) / (double)nw) ?  \
-           (((double)eh - v_margins) / (double)nh) :  \
-           (((double)ew - h_margins) / (double)nw);
 
+   if (mode)
+   {
+      switch (*mode)
+      {
+      case VIEW_ORIGINAL:
+         scale = 1.0;
+         break;
+      case VIEW_FIT:
+         scale = (((double) eh - v_margins) / (double) nh <        \
+                 ((double) ew - h_margins) / (double) nw) ?        \
+                 (((double) eh - v_margins) / (double) nh) :       \
+                 (((double) ew - h_margins) / (double) nw);
+         break;
+      case VIEW_HFIT:
+         scale = ((double) ew - h_margins) / (double) nw;
+         break;
+      case VIEW_VFIT:
+         scale = ((double) eh - v_margins) / (double) nh;
+         break;
+      case VIEW_CUSTOM:
+         scale = *(double *)ecore_evas_data_get(ecore_evas_ecore_evas_get(evas), "scale");
+         break;
+      default: // Just to shut up compiler
+         scale = 0.0;
+         break;
+      }
+   }
+   else
+   {
+      printf("Using default view mode\n");
+      scale = (((double) eh - v_margins) / (double) nh <        \
+              ((double) ew - h_margins) / (double) nw) ?        \
+              (((double) eh - v_margins) / (double) nh) :       \
+              (((double) ew - h_margins) / (double) nw);
+   }
 
    evas_object_resize(border, nw * scale, nh * scale);
-   if (scale == ((double)eh - v_margins) / (double)nh)
-      evas_object_move(border, ew / 2 - nw * scale / 2, data->top_margin);
-   else
-      evas_object_move(border, data->left_margin, eh / 2 - nh * scale / 2);
-   
+   evas_object_move(border, ew / 2 - nw * scale / 2, eh / 2 - nh * scale / 2);
+
    doc_scale_set(page, scale);
-   
+
    printf("Scale: %f, Native w: %d, h: %d\n", scale, nw, nh);
 
    if (data->render_timer) // We're still resizing, delete timer set on prev resize
@@ -286,7 +318,8 @@ page_resize_cb(void *_data, Evas *evas, Evas_Object *controls, void *event_info)
                                         page);
 }
 
-int page_resize_cb_render_timer(void *data)
+int
+page_resize_cb_render_timer(void *data)
 {
    doc_render((Evas_Object *) data);
    return 0;
